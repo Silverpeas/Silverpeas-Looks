b1ddb305fe1578d8bc5bc164cfd323e800f5911b
svuillet <svu@silvepeas.com>
2026-03-12 10:58:23 +0100

    Feature #14182

--- aurora/aurora-war/src/main/java/org/silverpeas/looks/aurora/matomo/TrackableContentProvider.java
+++ aurora/aurora-war/src/main/java/org/silverpeas/looks/aurora/matomo/TrackableContentProvider.java
@@ -1,17 +1,23 @@
 package org.silverpeas.looks.aurora.matomo;
 
+import org.silverpeas.components.blog.model.PostDetail;
+import org.silverpeas.components.blog.service.BlogService;
+import org.silverpeas.components.quickinfo.model.News;
+import org.silverpeas.components.quickinfo.model.QuickInfoService;
+import org.silverpeas.core.calendar.CalendarEvent;
 import org.silverpeas.core.contribution.publication.model.PublicationDetail;
 import org.silverpeas.core.contribution.publication.model.PublicationPK;
 import org.silverpeas.core.contribution.publication.service.PublicationService;
-
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TrackableContentProvider {
     public static TrackableContent get(HttpServletRequest httpReq) {
         String path = httpReq.getRequestURI();
         TrackableContent content = new TrackableContent();
         content.setContent(false);
-        //TODO : extract content with ApplicationServiceProvider
         if (path.contains("ViewPublication")) {
             content.setContentType("Publication");
             content.setContent(true);
@@ -23,9 +29,53 @@ public class TrackableContentProvider {
             content.setContentType("News");
             content.setContent(true);
             content.setContentId(httpReq.getParameter("Id"));
+            News n = QuickInfoService.get().getNews(content.getContentId());
+            content.setContentName(n.getTitle());
+            content.setPermalink(n.getPermalink());
+        } else if (path.contains("/CalendarEvent/") && !path.contains("possibledurations")) {
+            content.setContentType("Event");
+            content.setContent(true);
+            Pattern pattern = Pattern.compile("CalendarEvent/([a-f0-9\\-]+)");
+            Matcher matcher = pattern.matcher(path);
+            String id = "";
+            if (matcher.find()) {
+                id =  matcher.group(1);
+            }
+            content.setContentId(id);
+            content.setPermalink("/silverpeas/Contribution/" + id);
+            content.setContentName(CalendarEvent.getById(id).getTitle());
+        } else if (path.contains("Rdirectory")) {
+            content.setContentType("Directory");
+            content.setContent(true);
             content.setPermalink(path);
+            content.setContentName("Users directory");
+            content.setContentId("");
+        } else if (path.contains("RjobManagerPeas/jsp/Main")) {
+            content.setContentType("Administration");
+            content.setContent(true);
+            content.setPermalink(path);
+            content.setContentName("Admin console");
+            content.setContentId("");
+        } else if (path.contains("ViewPost")) {
+            String id = httpReq.getParameter("PostId");
+            String blogId = "";
+            Pattern pattern = Pattern.compile("Rblog/([^/]+)");
+            Matcher matcher = pattern.matcher(path);
+            if (matcher.find()) {
+                blogId = matcher.group(1);
+            }
+            content.setContentType("Post");
+            content.setContent(true);
+            content.setContentId(id);
+            Collection<PostDetail> posts = BlogService.get().getAllPosts(blogId);
+            for (PostDetail post : posts) {
+                if (post.getId().equals(id)) {
+                    content.setContentName(post.getTitle());
+                    content.setPermalink(post.getPermalink());
+                    break;
+                }
+            }
         }
-
         return content;
     }
 }
